3243acc0d07bda8985a0f09a26e5233dfc88f392
Cynep-SU <49202787+Cynep-SU@users.noreply.github.com>
2021-01-03 13:32:24 +0300

    Enemy almost ready

--- main.py
+++ main.py
@@ -52,15 +52,15 @@ def game():
             self.image = Player_live.original_image.copy()
             self.rect = self.image.get_rect(center=(x, y))
 
-
     class Enemy(pygame.sprite.Sprite):
         original_image = load_image('bad_guy.png')
-        original_image = pygame.transform.smoothscale(original_image, (70, 78))
-        speed = 5
+        original_image = pygame.transform.smoothscale(original_image, (45, 50))
 
-        def __init__(self, x, y):
+        def __init__(self, x, y, speed=3, lives=1):
             super().__init__(enemy_sprites)
             self.pos = (x, y)
+            self.speed = speed
+            self.lives = lives
             self.image = Enemy.original_image.copy()
             self.rect = self.image.get_rect(center=(x, y))
             self.mask = pygame.mask.from_surface(self.image)
@@ -69,15 +69,20 @@ def game():
             self.angle = ((180 / math.pi) * -math.atan2(step_to_y, step_to_x))
 
         def update(self):
+            if (self.rect.centerx > 500 or self.rect.centerx < 0) or (self.rect.centery > 500 or self.rect.centery < 0):
+                step_to_x = width // 2 - self.rect.centerx
+                step_to_y = height // 2 - self.rect.centery
+                self.angle = ((180 / math.pi) * -math.atan2(step_to_y, step_to_x))
+            if self.lives == 0:
+                self.kill()
             self.image = pygame.transform.rotate(self.original_image, int(self.angle))
-            if not pygame.sprite.collide_mask(self, player) and not pygame.sprite.collide_mask(self, tank):
-                self.rect.center = calculate_new_xy(self.rect.center, self.speed, math.radians(-self.angle))
+            if not pygame.sprite.collide_mask(self, player) and not pygame.sprite.collide_mask(self,
+                                                                                               tank):
+                self.rect.center = calculate_new_xy(self.rect.center, self.speed,
+                                                    math.radians(-self.angle))
             else:
                 player.lives -= 1
                 self.kill()
-
-
-
 
     class Attack(pygame.sprite.Sprite):
         original_image = load_image('attack.png')
@@ -104,8 +109,9 @@ def game():
         # original_image = pygame.transform.smoothscale(original_image, (118, 90))
         original_image = pygame.transform.smoothscale(original_image, (150, 48))
         angle = 0
-        lives = 1
-        lives_obj = [Player_live(width - 44, 36)]
+        lives = 3
+        lives_obj = [Player_live(width - 44 + 20, 20), Player_live(width - 44 * 2 + 20, 20),
+                     Player_live(width - 44 * 3 + 20, 20)]
 
         def __init__(self, x, y):
             super().__init__(player_sprites)
@@ -124,7 +130,9 @@ def game():
             self.rect = self.image.get_rect(center=self.pos)
 
         def update(self):
-            pass
+            if self.lives < len(self.lives_obj):
+                self.lives_obj[len(self.lives_obj) -1].kill()
+
 
     class Tank(pygame.sprite.Sprite):
         original_image = load_image('tank_1.png')
@@ -143,7 +151,9 @@ def game():
     tank = Tank(width // 2, height // 2)
     player = Player(width // 2, height // 2)
 
-    Enemy(0, 0)
+    Enemy(-50, -50)
+    SPAWNENEMYEVENT = pygame.USEREVENT + 3
+    pygame.time.set_timer(SPAWNENEMYEVENT, 2000)
     color = random_color()
     CHANGEBGEVENT = pygame.USEREVENT + 1
     CHANGEBGEVENT_SECOND = pygame.USEREVENT + 2
@@ -159,26 +169,36 @@ def game():
         enemy_sprites.draw(screen)
         if player.lives == 0:
             running = False
+        list = pygame.sprite.groupcollide(attack_sprites, enemy_sprites, True, False)
+        if list:
+            for key in list.keys():
+                for enemy in list[key]:
+                    enemy.lives -= 1
+                    print(enemy)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 Attack(width // 2, height // 2)
-            if event.type == pygame.MOUSEMOTION:
+            elif event.type == pygame.MOUSEMOTION:
                 if 150 >= math.hypot(pygame.mouse.get_pos()[0] - width // 2,
                                      pygame.mouse.get_pos()[1] - height // 2) <= 250:
                     mouse_last_pos = pygame.mouse.get_pos()
                 player.rotate(mouse_x=pygame.mouse.get_pos()[0], mouse_y=pygame.mouse.get_pos()[1])
-
-            if event.type == CHANGEBGEVENT:
+            elif event.type == SPAWNENEMYEVENT:
+                xy = (1, 1)
+                while (xy[0] > 0 and xy[1] > 0) or (xy[0] < 500 and xy[1] < 500):
+                    xy = (random.randint(-700, 700), random.randint(-700, 700))
+                print(xy)
+                Enemy(xy[0], xy[1])
+            elif event.type == CHANGEBGEVENT:
                 timer_start = False
                 next_color = random_color()
                 r_add = (next_color[0] - color[0]) / 2000
                 g_add = (next_color[1] - color[1]) / 2000
                 b_add = (next_color[2] - color[2]) / 2000
                 pygame.time.set_timer(CHANGEBGEVENT_SECOND, 16)
-            if event.type == CHANGEBGEVENT_SECOND and next_color != color:
+            elif event.type == CHANGEBGEVENT_SECOND and next_color != color:
                 if r_add < 0 and next_color[0] > color[0]:
                     r_add = 0
                 if g_add < 0 and next_color[1] > color[1]:
